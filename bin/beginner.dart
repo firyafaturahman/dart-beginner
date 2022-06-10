e025167f6ff14c9756068c2d0d8e5227c3693808
firya faturahman <firya.faturahman2017@gmail.com>
2022-06-10 21:14:26 +0700

    class & object

--- bin/beginner.dart
+++ bin/beginner.dart
@@ -1,3 +1,24 @@
+import 'oop/class_object.dart';
+
 void main(List<String> arguments) {
-  print('Hello world!');
+  // materi class & object
+  PersegiPanjang kotak1, kotak2;
+
+  // membuat object kotak1
+  kotak1 = PersegiPanjang();
+  kotak1.panjang = 8;
+  kotak1.lebar = 6;
+
+  // luas kotak1
+  double luasKotak1 = kotak1.hitungLuas();
+  print("Luas kotak1 = $luasKotak1");
+
+  // membuat object kotak2
+  kotak2 = PersegiPanjang();
+  kotak2.panjang = 9;
+  kotak2.lebar = 5;
+
+  // luas kotak2
+  double luasKotak2 = kotak2.hitungLuas();
+  print("Luas kotak2 = $luasKotak2");
 }
---
+++ bin/oop/class_object.dart
@@ -0,0 +1,8 @@
+class PersegiPanjang {
+  double panjang = 0;
+  double lebar = 0;
+
+  double hitungLuas() {
+    return panjang * lebar;
+  }
+}
